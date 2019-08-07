b1b5d9e11129596de9f48b3e2e2967d42ac224a5
Sybil <sdurrash@gmail.com>
2019-08-08 04:29:24 +0800

    updated email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ApplicationMailer
     
     def welcome_email(user)
         @user = user
-        @url  = 'http://example.com/login'
-        mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+        @url  = 'http://www.sybling.site/users/sign_in'
+        mail(to: @user.email, subject: 'Welcome to s-tutoring')
     end
 end
