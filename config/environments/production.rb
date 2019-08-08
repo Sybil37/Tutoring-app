88e60176e69092ccc2ec6c9f36fb72d846bd83b8
Sybil <sdurrash@gmail.com>
2019-08-08 17:08:34 +0800

    added a host in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,6 +21,8 @@ Rails.application.configure do
   #   domain: 'sandboxa1aa5184662d47dd942ce2242676770d.mailgun.org'
   # }
   # byebug
+  config.action_mailer.default_url_options = { host: 'www.sybling.site'}
+
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
